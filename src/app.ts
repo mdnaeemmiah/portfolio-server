b5321f3c16c2a44ad9541e494211cfcf98b6e076
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-05-15 14:55:23 +0600

    added

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://l2b4-a5-client.vercel.app', credentials: true }));
 
 // application routes
 app.use('/api', router);
